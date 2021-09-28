48b1fcc747da44d4766bcafc62345cb8c85e062d
sethu <sethu@DESKTOP-24CTG5U>
2021-09-28 12:02:57 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to TicTocToe");
+            TicTacToe ticTac = new TicTacToe();
+            ticTac.CreatingBoard();
+        }
+    }
+}
---
+++ TicTacToe.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    class TicTacToe
+    {
+        public void CreatingBoard()
+        {
+            char[] array = new char[10];
+            int i;
+            for (i = 1; i == array.Length; i++)
+            {
+                array[i] = ' ';
+            }
+            Console.WriteLine("Array is", +array[i]);
+        }
+    }
+}
